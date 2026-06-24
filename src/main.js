0b7d1e1ac2d1be599a4f18f56811a81551230eec
T1ag0o <arnhold.tiagoR@outlook.com>
2026-06-23 23:54:48 -0400

    Configuração do projeto. WEBPACK/BABEL e pacote Dayjs

---
+++ src/main.js
@@ -0,0 +1,9 @@
+"use strict"
+
+//Configuração do Dayjs
+import "./libs/dayjs"
+
+// buildar o CSS com o WEBPACK
+import "./styles/global.css"
+import "./styles/form.css"
+import "./styles/schedule.css"
